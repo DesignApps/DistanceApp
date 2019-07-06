b19af873d472cd9a13ed616b574df27f5271e495
Amy Graff <amygraff.psychemath@gmail.com>
2019-07-06 12:04:49 -0500

    Built UI and added labels to code.

--- DistanceApp/ViewController.m
+++ DistanceApp/ViewController.m
@@ -9,15 +9,29 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *startLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocation1;
+@property (weak, nonatomic) IBOutlet UILabel *distance1;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocation2;
+@property (strong, nonatomic) IBOutlet UIView *distance2;
+
+@property (weak, nonatomic) IBOutlet UITextField *endLocation3;
+@property (weak, nonatomic) IBOutlet UILabel *distance3;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)calculateButtonTapped:(id)sender {
+    
+    self.distance3.text = @"It worked";
 }
+
+
 
 
 @end
